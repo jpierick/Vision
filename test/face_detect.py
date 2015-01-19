8ec626a41449c9611f0c4dc5ee1369cfdd3b71e0
Jeff <jeff.pierick@gmail.com>
2015-01-18 19:32:10 -0800

    Write file with face

--- test/face_detect.py
+++ test/face_detect.py
@@ -1,7 +1,17 @@
+#!/usr/bin/python
+# ==============================================================================
+# Name: capture_training_images.py
+# Desc: This program is used to capture training images that will be used by
+#   the face recognition program.  
+# Change History:
+# 2015-02-01 JSP: Created.
+# ==============================================================================
+
 import cv2
 import sys
 import time
 
+# Verify that the cascade 
 cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
 
@@ -22,20 +32,22 @@ while True:
     flags=cv2.cv.CV_HAAR_SCALE_IMAGE
   )
 
+  # Put a blue box around all the faces that were found in the image:
   for (x, y, w, h) in faces:
     cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
     face_found = True
 
+  cv2.imshow('Video', frame)
+
   key = cv2.waitKey(1) & 0xFF
 
-  cv2.imshow('Video', frame)
-
   if face_found == True and key == ord('k'):
-    cv2.putText(frame, "Keep", (50,50), cv2.FONT_HERSHEY_PLAIN, 3.0, (0,0,255))    
-    time.sleep(5)
+    print "Keeping face"
+    for (x, y, w, h) in faces:
+      print "X:" + str(x) + ", Y:" + str(y) + ", W:" + str(w) + ", H:" + str(h)
+      cv2.imwrite('face.png', frame[y:y+h, x:x+w])
+    time.sleep(1)
     face_found = False
-
-  cv2.imshow('Video', frame)
 
   if key == ord('q'):
     break
