3368ff0c80049142ecf6e321191f0cb477e2ace4
Jeff <jeff.pierick@gmail.com>
2015-02-08 16:27:13 -0800

    Added video camera feed to facerec.py

--- facerec/facerec.py
+++ facerec/facerec.py
@@ -14,12 +14,14 @@
 # ==============================================================================
 
 from sklearn.decomposition import RandomizedPCA
-import numpy as np
-import glob
 import cv2
+import glob
 import math
+import numpy as np
 import os.path
 import string
+import sys
+import time
 
 # Ensure that the training subfolder exists:
 if not os.path.isdir('training'):
@@ -29,26 +31,26 @@ if not os.path.isdir('training'):
 # Read through all of the training images to determine their dimensions and
 # to ensure that there is at least one training image.  If any of the images
 # have different dimensions than the others, then report the error and quit:
-imageCount = 0
+imgTrainingCountTotal = 0
 height = 0
 width = 0
 depth = 0
 minImagesPerFolder = 0
 maxImagesPerFolder = 0
 for folderName in os.listdir('training'):
-  imagesInFolder = 0
-  subfolderName = os.path.join('training', folderName)
-  if os.path.isdir(subfolderName):
-    for fileName in os.listdir(subfolderName):
+  imgCountInFolder = 0
+  folderFullName = os.path.join('training', folderName)
+  if os.path.isdir(folderFullName):
+    for fileName in os.listdir(folderFullName):
       if os.path.splitext(os.path.basename(fileName))[0].isdigit():
         # Keep track of the number of training images:
-        imageCount = imageCount + 1
-        imagesInFolder = imagesInFolder + 1
+        imgTrainingCountTotal = imgTrainingCountTotal + 1
+        imgCountInFolder = imgCountInFolder + 1
 
         # Read the image so that it can be processed:
-        img = cv2.imread(os.path.join(subfolderName, fileName))
+        img = cv2.imread(os.path.join(folderFullName, fileName))
         if img == None: 
-          print "Unable to read image " + os.path.join(subfolderName, fileName)
+          print "Unable to read image " + os.path.join(folderFullName, fileName)
           quit(1)
 
         # Determine the images dimensions and ensure that they are the same
@@ -59,21 +61,21 @@ for folderName in os.listdir('training'):
           width = imgWidth
           depth = imgDepth
         elif imgHeight <> height or imgWidth <> width or imgDepth <> depth:
-          print "File " + fileName + " in folder " + subfolderName + \
+          print "File " + fileName + " in folder " + folderFullName + \
             " has different dimensions than files previously read."
           quit(1)
 
   # Track the minimum and maximum number of training images in all the 
   # folders:
   if minImagesPerFolder == 0:
-    minImagesPerFolder = imagesInFolder
-  elif imagesInFolder < minImagesPerFolder:
-    minImagesPerFolder = imagesInFolder
+    minImagesPerFolder = imgCountInFolder
+  elif imgCountInFolder < minImagesPerFolder:
+    minImagesPerFolder = imgCountInFolder
 
-  if imagesInFolder > maxImagesPerFolder:
-    maxImagesPerFolder = imagesInFolder
+  if imgCountInFolder > maxImagesPerFolder:
+    maxImagesPerFolder = imgCountInFolder
 
-if imageCount == 0:
+if imgTrainingCountTotal == 0:
   print "Couldn't find any training images."
   quit(1)
 
@@ -82,15 +84,15 @@ print "Maximum number of training images for a subject: " + str(maxImagesPerFold
 
 # Create an array of flattened training images and a corresponding array of
 # names (the name associated with each corresponding training image):
-trainingImages = np.zeros([imageCount, height * width], dtype='int8')
+trainingImages = np.zeros([imgTrainingCountTotal, height * width], dtype='int8')
 trainingName = []
 
 # Populate the arrays with the flattened training images:
 imageNumber = 0
 for folderName in os.listdir('training'):
-  subfolderName = os.path.join('training', folderName)
-  if os.path.isdir(subfolderName):
-    for fileName in os.listdir(subfolderName):
+  folderFullName = os.path.join('training', folderName)
+  if os.path.isdir(folderFullName):
+    for fileName in os.listdir(folderFullName):
       if os.path.splitext(os.path.basename(fileName))[0].isdigit():
         print "Processing image " + fileName + " for " + folderName
 
@@ -98,9 +100,9 @@ for folderName in os.listdir('training'):
         trainingName.append(folderName)
 
         # Read the image so that it can be processed:
-        img = cv2.imread(os.path.join(subfolderName, fileName))
+        img = cv2.imread(os.path.join(folderFullName, fileName))
         if img == None: 
-          print "Unable to read image " + os.path.join(subfolderName, fileName)
+          print "Unable to read image " + os.path.join(folderFullName, fileName)
           quit(1)
 
         img = cv2.cvtColor(img, cv2.cv.CV_RGB2GRAY)
@@ -108,25 +110,71 @@ for folderName in os.listdir('training'):
         imageNumber = imageNumber + 1
     
 # Perform principal component analysis on the images:
-print "Performing principal component analysis."
+print "Performing principal component analysis of training images."
 pca = RandomizedPCA(n_components=maxImagesPerFolder, whiten=True).fit(trainingImages)
 pcaTrainingImages = pca.transform(trainingImages)
 
-# Load the test image
-testImages = np.zeros([1, height*width], dtype='int8')
-img = cv2.imread('1.png')
-img = cv2.cvtColor(img, cv2.cv.CV_RGB2GRAY)
-testImages[0,:] = img.flat
 
-for j, ref_pca in enumerate(pca.transform(testImages)):
-  distances = []
-  for i, test_pca in enumerate(pcaTrainingImages):
-    dist = math.sqrt(sum([diff**2 for diff in (ref_pca - test_pca)]))
-    distances.append((dist, trainingName[i]))
+# Verify that the cascade file exists that will be used to detect faces in the
+# webcam feed:
+cascPath = './haarcascade_frontalface_default.xml'
+if not os.path.isfile(cascPath):
+  print 'The face recognition cascade file (' + cascPath + ') does not exist.'
+  quit(1)
 
-  found_ID = min(distances)[1]
-  print "Identified (result: " + found_ID + " - dist - " + str(min(distances)[0]) + ")"
+faceCascade = cv2.CascadeClassifier(cascPath)
 
+# Specify which camera to use to capture the video stream from which faces
+# will be detected:
+video_capture = cv2.VideoCapture(0)
 
-quit(0)
+while True:
+  # On each loop, we check to see if any faces were detected in the image.
+  # If one (or more) was, then the user is given the opportunity to keep
+  # the image (the grayscale version) as a training image:
+  face_found = False
+
+  ret, frame = video_capture.read()
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+  faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+  )
+
+  # Put a green box around all the faces that were found in the image:
+  for (x, y, w, h) in faces:
+    cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
+    face_found = True
+
+  cv2.imshow('Video', frame)
+  key = cv2.waitKey(1) & 0xFF
+  if key == ord('q'):
+    break
+
+  if face_found == True:
+    for (x, y, w, h) in faces:
+      face_img = gray[y:y+h, x:x+w]
+      face_img = cv2.resize(face_img, (100,100), interpolation=cv2.INTER_CUBIC)
+      face_img = cv2.equalizeHist(face_img)
+
+      testImages = np.zeros([1, 100*100], dtype='int8')
+      testImages[0,:] = face_img.flat
+
+      for j, ref_pca in enumerate(pca.transform(testImages)):
+        distances = []
+        for i, test_pca in enumerate(pcaTrainingImages):
+          dist = math.sqrt(sum([diff**2 for diff in (ref_pca - test_pca)]))
+          distances.append((dist, trainingName[i]))
+  
+        found_ID = min(distances)[1]
+        print "Identified (result: " + found_ID + " - dist - " + str(min(distances)[0]) + ")"
+
+        face_found = False
+
+video_capture.release()
+cv2.destroyAllWindows()
 
